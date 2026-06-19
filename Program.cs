fd99f733633ac1c4be83e99240c1158b1a1aba5f
Per Olsen <persteen22@gmail.com>
2026-06-19 13:08:06 +0200

    Updated the Date of Readme and User Objects

--- Program.cs
+++ Program.cs
@@ -66,7 +66,7 @@ using (var scope = app.Services.CreateScope())
     var testUser = new User
     {
         FirstName = "Test",
-        LastName = "User - 09-06-2026",
+        LastName = "User - 19-06-2026",
         Username = "test",
         PasswordHash = BCrypt.Net.BCrypt.HashPassword("test")
     };
@@ -76,7 +76,7 @@ using (var scope = app.Services.CreateScope())
     var testAdmin = new User
     {
         FirstName = "Admin",
-        LastName = "User - 09-06-2026",
+        LastName = "User - 19-06-2026",
         Username = "admin",
         PasswordHash = BCrypt.Net.BCrypt.HashPassword("admin")
     };
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 # Last Updated
 
-- 09-06-2026
+- 19-06-2026
 
 The Web API was made without the ASP.NET Core Identity using custom JWT middleware
 
